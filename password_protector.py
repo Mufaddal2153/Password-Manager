4569c47faec3757683ac31035506a4a7e359339c
Mufaddal2153 <39365711+Mufaddal2153@users.noreply.github.com>
2020-08-09 18:34:59 +0500

    Update password_protector.py

--- password_protector.py
+++ password_protector.py
@@ -11,73 +11,76 @@ else:
     open(file, "w+")
     passwordsFile = {}
 
-def password_Locker():
-    print("Password Locker")
-    print("1. Find your Password")
-    print("2. Save your Password")
-    print("3. Update pass")
-    print("4. Exit")
+masterPass = input("Enter the password")
+while masterPass == "mufaddal":
+    def password_Locker():
+        print("Password Locker")
+        print("1. Find your Password")
+        print("2. Save your Password")
+        print("3. Update pass")
+        print("4. Exit")
 
-    try:
-        optionNumber = int(input("Enter your option number:"))
-    except:
-        print("Enter the provided Number")
-        optionNumber = int(input("Enter your option number:"))
-    while optionNumber:
-        if optionNumber == 1:
-            passFinder()
-        elif optionNumber == 2:
-            passSave()
-        elif optionNumber == 3:
-            passUpdate()
-        elif optionNumber == 4:
-            break
+        try:
+            optionNumber = int(input("Enter your option number:"))
+        except:
+            print("Enter the provided Number")
+            optionNumber = int(input("Enter your option number:"))
+        while optionNumber:
+            if optionNumber == 1:
+                passFinder()
+            elif optionNumber == 2:
+                passSave()
+            elif optionNumber == 3:
+                passUpdate()
+            elif optionNumber == 4:
+                break
+            else:
+                print("invalid option")
+            optionNumber = int(input("Enter your option number:"))
+        print("Now, Exiting")
+
+
+    def passFinder():
+        acc = input("Enter your email or platform")
+
+        if acc in passwordsFile.keys():
+            passEncrypted = passwordsFile[acc]
+            pyperclip.copy(decode(passEncrypted))
+            print("Password copied... 60 sec!!!")
+            time.sleep(60)
+            print("Password Erased from clip board")
         else:
-            print("invalid option")
-        optionNumber = int(input("Enter your option number:"))
-    print("Now, Exiting")
+            print("Invalid Account")
 
+    def passUpdate():
+        acc = input("Enter your email or platform")
+        if acc in passwordsFile.keys():
+            newpass = input("Enter new password: ")
+            passwordsFile[acc] = encode(newpass)
+            json.dump(passwordsFile, open(file, "w"))
+            print("New Password Saved")
 
-def passFinder():
-    acc = input("Enter your email or platform")
+    def decode(passEncrypted):
+        decodedPass = []
+        for i in range(len(passEncrypted)):
+            decodePass = chr(ord(passEncrypted[i]) + len(passEncrypted))
+            decodedPass.append(decodePass)
+        passDecoded = "".join(decodedPass)
+        return passDecoded
 
-    if acc in passwordsFile.keys():
-        passEncrypted = passwordsFile[acc]
-        pyperclip.copy(decode(passEncrypted))
-        print("Password copied... 60 sec!!!")
-        time.sleep(60)
-        print("Password Erased from clip board")
-    else:
-        print("Invalid Account")
-
-def passUpdate():
-    acc = input("Enter your email or platform")
-    if acc in passwordsFile.keys():
-        newpass = input("Enter new password: ")
-        passwordsFile[acc] = encode(newpass)
+    def passSave():
+        acc = input("Enter your email or platform")
+        password = input("Enter your password to add")
+        passwordsFile[acc] = encode(password)
         json.dump(passwordsFile, open(file, "w"))
-        print("New Password Saved")
+        print("Password Saved")
+    def encode(password):
+        encodedPass = []
+        for i in range(len(password)):
+            encodePass = chr(ord(password[i]) - len(password))
+            encodedPass.append(encodePass)
+        passEncrypted = "".join(encodedPass)
+        return passEncrypted
 
-def decode(passEncrypted):
-    decodedPass = []
-    for i in range(len(passEncrypted)):
-        decodePass = chr(ord(passEncrypted[i]) + len(passEncrypted))
-        decodedPass.append(decodePass)
-    passDecoded = "".join(decodedPass)
-    return passDecoded
-
-def passSave():
-    acc = input("Enter your email or platform")
-    password = input("Enter your password to add")
-    passwordsFile[acc] = encode(password)
-    json.dump(passwordsFile, open(file, "w"))
-    print("Password Saved")
-def encode(password):
-    encodedPass = []
-    for i in range(len(password)):
-        encodePass = chr(ord(password[i]) - len(password))
-        encodedPass.append(encodePass)
-    passEncrypted = "".join(encodedPass)
-    return passEncrypted
-
-password_Locker()
+    password_Locker()
+print("incorrect Password")
